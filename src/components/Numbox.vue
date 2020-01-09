0b702423f01423343f5b59f39dfcc3d83d21a055
Mr Hong <857329255@qq.com>
2020-01-09 18:04:15 +0800

    2020年01月09日

--- src/components/Numbox.vue
+++ src/components/Numbox.vue
@@ -1,8 +1,9 @@
 <template>
     <div id="numbox-container">
-        <div class="mui-numbox" data-numbox-min='1' data-numbox-max='9'>
+        <!-- 由于父组件传值进来的 countMax属性值是异步加载, 会导致在还未得到此属性值时, 就渲染了此组件 导致countMax值为undefined; 利用 watch 来监听 父组件传过来的 countMax值, 不管watch会被触发几次, 但最后一次肯定是一个合法的数值 -->
+        <div class="mui-numbox" data-numbox-min='1' :data-numbox-max='max'>
             <button class="mui-btn-numbox-minus" type="button">-</button>
-            <input class="mui-input-numbox" type="number" value="1" />
+            <input ref="box" class="mui-input-numbox" type="number" value="1" @change="textChange"/>
             <button class="mui-btn-numbox-plus" type="button">+</button>
         </div>
     </div>
@@ -18,10 +19,40 @@
             // 为munberbox类的div
             mui(".mui-numbox").numbox();
         },
-        data() {
-            return {};
+        props: {
+            getGoodsCount: Function,
+            // 库存的最大值
+            countMax: String | Number
         },
-        methods: {},
+        data() {
+            return {
+                
+            };
+        },
+        methods: {
+            // 监听input的改变
+            textChange() {
+                this.$emit('getGoodsCount', parseInt(this.$refs.box.value));
+            }
+        },
+        // 动态监听属性值改变
+        watch: {
+            // ES5写法函数
+            // countMax: function(newValue, oldValue) {
+            //     mui('.mui-numbox').numbox().setOption('max', newValue);
+            // }
+
+            // ES6箭头函数
+            // countMax: (newValue, oldValue)=>{
+            //     // 设置最大值 mui中JS API
+            //     mui('.mui-numbox').numbox().setOption('max', newValue);
+            // }
+
+            countMax: (newValue, oldValue)=>{
+                console.log("新值:", newValue);
+                
+            }
+        }
     };
 </script>
 
--- src/pages/home/GoodsInfo.vue
+++ src/pages/home/GoodsInfo.vue
@@ -1,5 +1,15 @@
 <template>
     <div id="goodsInfo-container">
+
+        <!-- 购物红点; 实现半场动画 -->
+        <transition
+        @before-enter="beforeEnter"
+        @enter="enter"
+        @after-enter="afterEnter"
+        >
+            <div class="ball" ref="ball" v-show="ballFlag"></div>
+        </transition>
+
         <!-- 顶部 轮播 -->
         <div class="mui-card">
             <div class="mui-card-content">
@@ -22,7 +32,9 @@
                     </p>
 
                     <div class="buy-count">
-                        购买数量：<NumberBox></NumberBox>
+                        <!-- 由于 goodInfo对象是请求得到(异步请求), 有可能导致 NumberBox 被渲染后, 请求还未返回. 导致传进去的countMax 属性值为undefine; 解决: 在 NumberBox 中利用watch来监听此 countMax属性 -->
+                        购买数量：<NumberBox :countMax="goodInfo.stock_quantity" @getGoodsCount="selGoodsCount"></NumberBox>
+                        <van-icon name="cart-circle-o" info="0"/>
                     </div>
 
                     <div class="buttons">
@@ -60,12 +72,14 @@
     import Network from '../../utils/Network';
     import Carousel from '../../components/Carousel';
     import NumberBox from '../../components/Numbox';
+    import { Icon } from 'vant';
 
     export default {
         name: "goodsInfo",
         components: {
             Carousel,
-            NumberBox
+            NumberBox,
+            [Icon.name]: Icon
         },
         created() {
             this.getCarousels();
@@ -75,7 +89,9 @@
             return {
                 goodsId: this.$route.params.goodsId,
                 carousels: [],
-                goodInfo: {}
+                goodInfo: {},
+                ballFlag: false,
+                goodsCount: 1,
             };
         },
         methods: {
@@ -111,7 +127,7 @@
             },
             // 加入购物车
             addCart() {
-                
+                this.ballFlag = !this.ballFlag;
             },
 
             // 图文介绍
@@ -127,6 +143,50 @@
                     name: 'goodscomment',
                     params: { goodsId: this.goodsId }
                 }) 
+            },
+
+            // 获取numberBox中的数量
+            selGoodsCount(count) {
+                this.goodsCount = count;
+
+                console.log('父控件数量:', count);
+            },
+
+            // 小红点进入之前动画
+            beforeEnter(el) {
+                // 摆正红点(由于后面红点位置会发生改变)
+                el.style.transform = 'translate(0, 0)';
+            },
+            // 小红点进入了的动画
+            enter(el, done) {
+                // 动画关键代码
+                el.offsetWidth;
+                // 需要什么动画效果过渡
+                el.style.transition = 'all 1s cubic-bezier(0.5, -0.5, 1, 1)';
+                // 红点位移的终点
+                el.style.transform = 'translate(172px, -9px)';
+                // done回调函数就是对对应事件enter事件 之后afterenter事件的引用
+                done()
+
+                // 问题: 当页面滚动后小球最终位移坐标不准确? 
+                // 解决: 小球动画位移不准确问题, 根据不同情况,动态计算最终坐标值!
+                // 步骤: 1.先得到最终目标元素的横纵坐标, 再得到小球横纵坐标, 让x, y值求差, 得到获得最终的(x, y)
+                // 2. 动态获取 目标 与 小球的位置?  解: 利用 domObject.getBoundingClinetRect() 返回矩形对象, 包含 left, top, right, bottom; 都是相对于屏幕的左上角(0, 0)距离
+
+                // 1. 获取小球在页面中的位置
+                // const ballOrigin = this.$refs.ball.getBoundingClientRect();
+                // 2. 当一个组件A不属于当前组件B时, 如何拿到此组件A, 利用DOM操作(通过DOM操作可以拿到页面中任何显示出来的组件); 设置target标签的id="", 例如设置成 id="badge"
+                // const targetOrigin = document.getElementById('badge').getBoundingClientRect();
+                // 3. 获取差值
+                // const xDist = targetOrigin.left - ballOrigin.left;
+                // const yDist = targetOrigin.top - ballOrigin.top;
+                // 4. 设置坐标
+                // el.style.transform = `translate(${xDist}px, ${yDist}px)`;
+
+            },
+            // 小红点离开之后
+            afterEnter(el) {
+                this.ballFlag = !this.ballFlag;
             }
         }
     };
@@ -136,6 +196,7 @@
     #goodsInfo-container {
         background-color: #eee;
         overflow: hidden;
+        position: relative;
 
         .price {
             color: black;
@@ -149,6 +210,16 @@
         .buy-count {
             display: flex;
             align-items: center;
+            position: relative;
+
+            .van-icon {
+                font-size: 25px;
+                position: absolute;
+                right: 20px;
+            }
+            .van-info {
+                top: 2px;
+            }
         }
         .buttons {
             margin-top: 10px;
@@ -159,6 +230,16 @@
         }
         .mui-card-footer {
             display: block;
+        }
+        .ball {
+            z-index: 99;
+            width: 15px;
+            height: 15px;
+            border-radius: 50%;
+            background-color: red;
+            position: absolute;
+            top: 350px;
+            left: 150px;
         }
     }
 </style>
